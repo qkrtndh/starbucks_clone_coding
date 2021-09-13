928b19b81a3c9fb6fa4bc578216a4e76523a56b8
qkrtndh <qkrtndh5@naver.com>
2021-09-13 09:20:33 +0900

    Starbucks clone coding

---
+++ js/main.js
@@ -0,0 +1,75 @@
+const searchEl = document.querySelector(".search");
+const inputEl = searchEl.querySelector(".search input");
+
+searchEl.addEventListener("click", function () {
+  inputEl.focus();
+});
+
+inputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  inputEl.setAttribute("placeholder", "통합검색");
+});
+
+inputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  inputEl.setAttribute("placeholder", "");
+});
+
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
+// 페이지 스크롤에 영향을 받는 요소들을 검색!
+const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    // 페이지 스크롤 위치가 500px이 넘으면.
+    if (window.scrollY > 500) {
+      // Badge 요소 숨기기!
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+      // 상단으로 스크롤 버튼 보이기!
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
+
+      // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    } else {
+      // Badge 요소 보이기!
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+      // 상단으로 스크롤 버튼 숨기기!
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
+    }
+  }, 300)
+);
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener("click", function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+  // 각 요소들을 순서대로(delay) 보여지게 함!
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
